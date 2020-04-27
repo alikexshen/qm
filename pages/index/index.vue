700533788e1018fa2e28462e3ed8eb5ac4e85fd3
Johnny <949394771@qq.com>
2020-04-27 19:08:12 +0800

    merge

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,46 +1,48 @@
 <template>
-	<view class="content">
-		<hx-navbar :back="false" :backgroundColorLinearDeg='90' :backgroundColor="[[71,204,160],[0,117,113]]" >
-			<view class="out fz24 pl50" slot="right">
-				退出
-			</view>
-		</hx-navbar>
-		<view class="topcont">
-			vjdj
-		</view>
-	</view>
+  <view class="content">
+    <hx-navbar :back="false" :backgroundColorLinearDeg='90' :backgroundColor="[[71,204,160],[0,117,113]]">
+      <view class="out fz24 pl50" slot="right">
+        退出
+      </view>
+    </hx-navbar>
+    <view class="topcont">
+      vjdj
+    </view>
+  </view>
 </template>
 
 <script>
-	import hxNavbar from "@/components/hx-navbar/hx-navbar.vue"
-	export default {
-		components: {hxNavbar},
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+  import hxNavbar from "@/components/hx-navbar/hx-navbar.vue"
+  export default {
+    components: {
+      hxNavbar
+    },
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {
 
-		},
-		methods: {
+    },
+    methods: {
 
-		}
-	}
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-	.content {
-		width: 100%;
-		.out{
-			color:#fff;
-		}
-		.topcont{
-			height: 310upx;
-			width:100% ;
-			background:linear-gradient(90deg,rgba(71,204,160,1) 0%,rgba(0,117,113,1) 100%);
-		}
-	}
+  .content {
+    width: 100%;
 
-	
+    .out {
+      color: #fff;
+    }
+
+    .topcont {
+      height: 310upx;
+      width: 100%;
+      background: linear-gradient(90deg, rgba(71, 204, 160, 1) 0%, rgba(0, 117, 113, 1) 100%);
+    }
+  }
 </style>
